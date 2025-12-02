adda49e17c26d08dac322e42541360061083fd89
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-12-02 16:17:20 +0100

    Add initial project structure and implementation for Oefening 1.2
    - Created .vscode/settings.json to set the default solution.
    - Added Oefening 1.2 project file (Oefening 1.2.csproj) with .NET 9.0 target framework.
    - Implemented a simple console application in Program.cs that outputs "Hello, World!".
    - Generated necessary assembly attributes and info files for the project.
    - Created solution file (Oefeningen.sln) to include the new project.
    - Updated Oefeningen project with similar structure and a sample implementation.
    - Added various autogenerated files for build and package management.

---
+++ Oefeningen/Program.cs
@@ -0,0 +1,17 @@
+﻿// Oefening 1.1
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        // Variabelen // 
+        string naam = "Jemimah";
+        int leeftijd = 17;
+        double gemiddelde = 90.4;
+        bool heeftRijbewijs = false;
+        char eersteLetter = naam[0];
+
+        System.Console.WriteLine
+        ($"Naam: {naam}\n Leeftijd: {leeftijd}\n Gemiddelde: {gemiddelde}%\n Heeft rijbewijs?: {heeftRijbewijs}\n Eerste letter van voornaam: {eersteLetter}");
+    }
+}
