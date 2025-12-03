741c67bca9eadc5d8112c5ffba3d551a3198e356
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-12-03 21:42:39 +0100

    Add project files and dependencies for Oefening 3.1
    - Created Oefening 3.1.dll and Oefening 3.1.csproj files for .NET 9.0.
    - Added NuGet package restore configuration and project assets.
    - Included necessary XML configuration files for project management.
    - Established project structure with appropriate paths and settings.

--- Oefening 2.2/obj/Debug/net9.0/Oefening 2.2.AssemblyInfo.cs
+++ Oefening 2.2/obj/Debug/net9.0/Oefening 2.2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oefening 2.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7827d378501f0eb847133a944f9141bab200ccae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883e4a3971ff1426b22a8cef4bf0c9bc065dc22c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oefening 2.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oefening 2.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Oefening 3.1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Oefening 3.1: Rekenmachine basis //
+
+/* Maak een simpel programma dat twee getallen vraagt en alle basisoperaties uitvoert: optellen,
+aftrekken, vermenigvuldigen, delen en modulo. Toon alle resultaten. */
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Input // 
+        Console.Write("Voer getal 1 in: ");
+        double getal1 = double.Parse(Console.ReadLine());
+        Console.Write("Voer getal 2 in: ");
+        double getal2 = double.Parse(Console.ReadLine());
+
+        // Berekeningen //
+        double som = getal1 + getal2;
+        double min = getal1 - getal2;
+        double product = getal1 * getal2;
+        double quotient = getal1 / getal2;
+        double modulo = getal1 % getal2;
+
+        // Output // 
+        Console.WriteLine
+        ($"Getal 1: {getal1}; Getal 2; {getal2}");
+        Console.WriteLine($"De som van {getal1} en {getal2} = {som}");
+        Console.WriteLine($"De verschil tussen {getal1} en {getal2} = {min}");
+        Console.WriteLine($"De product van {getal1} en {getal2} = {product}");
+        Console.WriteLine($"De quotient van {getal1} en {getal2} = {Math.Floor(quotient)}");
+        Console.WriteLine($"De restnadeling van {getal1} en {getal2} = {modulo}");
+    }
+}
+
+// Versie 2
