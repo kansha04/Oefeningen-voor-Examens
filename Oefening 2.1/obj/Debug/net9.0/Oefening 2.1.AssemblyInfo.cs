a04f78bd9e5e6436f31719458a30252b0a240ac1
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-12-04 08:40:31 +0100

    Added new oefening, updated files.

--- Oefening 2.1/obj/Debug/net9.0/Oefening 2.1.AssemblyInfo.cs
+++ Oefening 2.1/obj/Debug/net9.0/Oefening 2.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oefening 2.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be163b265fe896acd21927fb1a556c5449b446f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31ba9a089eef960290cc57b51efc3157c30c69f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oefening 2.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oefening 2.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Oefening 3.3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Oefening 3.3 BTW berekenen //
+
+/* Vraag de gebruiker om een prijs exclusief BTW. Bereken en toon:
+    Het BTW-bedrag (21%)
+    De totaalprijs inclusief BTW
+    Gebruik de += operator om de BTW bij de prijs op te tellen! 
+*/
+
+// Input //
+Console.Write("Prijs exclusief btw: ");
