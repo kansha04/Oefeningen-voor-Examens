dd11e78e69604e6c712c6d71ffdcbf0a5dc687b5
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-12-05 17:36:46 +0100

    Add project files and initial build artifacts for Oefening 4.2
    - Created DLL references for the project.
    - Added NuGet configuration files and project assets.
    - Included project metadata and assembly information.
    - Established package management settings and restore configurations.
    - Set up project structure for .NET 9.0 compatibility.

--- Oefening 2.1/obj/Debug/net9.0/Oefening 2.1.AssemblyInfo.cs
+++ Oefening 2.1/obj/Debug/net9.0/Oefening 2.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oefening 2.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16bb76c6bd5397bbe4fef65b0914aada287054e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aed6dd824010b03fec65a1884ef5c2506c6a167")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oefening 2.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oefening 2.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Oefening 4.2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Oefening 4.2: Dobbelsteenworp simulatie
+
+/* Simuleer het werpen van twee dobbelstenen. Toon:
+Het resultaat van dobbelsteen 1
+Het resultaat van dobbelsteen 2
+Het totaal
+Of de gebruiker een "dubbel" heeft gegooid (beide dobbelstenen hetzelfde getal)
+Tip: Gebruik Random.Next(1, 7) voor getallen tussen 1 en 6. */
+
+Console.WriteLine("Dobbelsteen werper");
+
+// Variabelen //
+
+Random randomGen = new Random();
+
+int roll1 = 0;
+int roll2 = 1;
+int attempts = 0;
+
+Console.WriteLine("Druk op enter om de dobbelsteen te gooien.");
+// While loop //
+
+do
+{
+    Console.ReadKey();
+
+    roll1 = randomGen.Next(1, 7);
+    roll2 = randomGen.Next(1, 7);
+
+    System.Console.WriteLine($"Dobbelsteen 1: {roll1}");
+    System.Console.WriteLine($"Dobbelsteen 2: {roll2}");
+    attempts++;
+} while (roll1 != roll2);
+
+Console.WriteLine($"Er waren in totaal {attempts} worpen nodig om een dubbel te gooien.");
