0aed6dd824010b03fec65a1884ef5c2506c6a167
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-12-05 11:33:16 +0100

    Add initial project files and configurations for Oefening 4.1
    - Created Oefening 4.1.dll and associated debug files.
    - Added project.assets.json for dependency management.
    - Included NuGet configuration files for package restoration.
    - Established project properties and framework settings for .NET 9.0.
    - Configured assembly attributes and security settings in the manifest.

---
+++ Oefening 4.1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Oefening 4.1: Wiskundige berekeningen //
+
+/* Vraag de gebruiker om een getal. Bereken en toon:
+Het getal tot de macht 2, 3 en 4 (gebruik Math.Pow() )
+De vierkantswortel van het getal (gebruik Math.Sqrt() )
+Het getal afgerond naar boven en naar beneden */
+
+// Input //
+Console.Write("Geef een getal: ");
+double getal = double.Parse(Console.ReadLine());
+
+// Berekeningen 
+double tweedeMacht = Math.Pow(getal, 2);
+double derdeMacht = Math.Pow(getal, 3);
+double vierdeMacht = Math.Pow(getal, 4);
+double vierkantswortel = Math.Sqrt(getal);
+double afgerond = Math.Round(getal);
+
+// Output //
+Console.Clear();
+
+Console.WriteLine($"Het getal {getal} tot de 2de macht = {tweedeMacht}");
+Console.WriteLine($"Het getal {getal} tot de 3de macht = {derdeMacht}");
+Console.WriteLine($"Het getal {getal} tot de 4de macht = {vierdeMacht}");
+Console.WriteLine($"Vierkantswortel {getal} = {Math.Round(vierkantswortel, 3)}");
+Console.WriteLine($"Getal afgerond = {afgerond}");
+
