e287084798e6fec345a2d2376e02497a59c1967c
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-12-05 10:37:46 +0100

    Refactor code structure and formatting in Oefening 3.4

--- Oefening 3.4/Program.cs
+++ Oefening 3.4/Program.cs
@@ -1,18 +1,25 @@
 ﻿// Oefening 3.4  Even of oneven //
- /* 
-    Vraag de gebruiker om een getal. Gebruik de modulo operator (%) om te bepalen of het getal
-    even of oneven is. Toon het resultaat.
- */
+/* 
+   Vraag de gebruiker om een getal. Gebruik de modulo operator (%) om te bepalen of het getal
+   even of oneven is. Toon het resultaat.
+*/
 
-// Input 
-Console.Write("Geef een getal: ");
-double getal = double.Parse(Console.ReadLine());
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Input 
+        Console.Write("Geef een getal: ");
+        double getal = double.Parse(Console.ReadLine());
 
-// Modulo
-if (getal % 2 == 0)
-{
-    Console.WriteLine($"Het getal {getal} is even");
-} else
-{
-    Console.WriteLine($"Het getal {getal} is oneven");
+        // Modulo
+        if (getal % 2 == 0)
+        {
+            Console.WriteLine($"Het getal {getal} is even");
+        }
+        else
+        {
+            Console.WriteLine($"Het getal {getal} is oneven");
+        }
+    }
 }
