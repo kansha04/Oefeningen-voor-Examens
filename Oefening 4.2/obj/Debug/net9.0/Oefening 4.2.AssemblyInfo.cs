7119026915e47fc9f56341f61897c2ada4e6b1c5
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-12-05 17:37:55 +0100

    chore: Update binaries and assembly info for Oefening 4.2

--- Oefening 4.2/obj/Debug/net9.0/Oefening 4.2.AssemblyInfo.cs
+++ Oefening 4.2/obj/Debug/net9.0/Oefening 4.2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oefening 4.2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd11e78e69604e6c712c6d71ffdcbf0a5dc687b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a2e762e91625debbc06cbb19b6b24111ddec98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oefening 4.2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oefening 4.2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
