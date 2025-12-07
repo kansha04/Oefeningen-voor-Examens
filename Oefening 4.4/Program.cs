6edf1696dc75a3e3fe77e2d8bb1ea64af732fd6a
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-12-07 21:53:39 +0100

    New project files added Oefening 4.4

---
+++ Oefening 4.4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Oefening 4.4: Wachtwoord validatie //
+/*  Vraag de gebruiker om een wachtwoord in te voeren. Controleer:
+Of het wachtwoord minstens 8 karakters lang is (.Length )
+Of het spaties bevat (.Contains(" ") )
+Toon of het wachtwoord geldig is of niet */ 
+
+// Input //
+Console.Write("Voer een wachtwoord in: ");
--- Oefeningen/obj/Debug/net9.0/Oefeningen.AssemblyInfo.cs
+++ Oefeningen/obj/Debug/net9.0/Oefeningen.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oefeningen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb67c4c5b103bb4e42b06f99458365209298f81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76a0db649077ab1d9caf52b1d8476355e4704b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oefeningen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oefeningen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
