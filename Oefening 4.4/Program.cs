13289d9463e4bc78ff2372a7e20154127a2119fc
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-12-09 19:53:41 +0100

    Finished with Oefening 4.4 and updated files

--- Oefening 4.4/Program.cs
+++ Oefening 4.4/Program.cs
@@ -5,4 +5,23 @@ Of het spaties bevat (.Contains(" ") )
 Toon of het wachtwoord geldig is of niet */ 
 
 // Input //
-Console.Write("Voer een wachtwoord in: ");
+Console.Write("Voer een wachtwoord in (minstens 8 karakters): ");
+string wachtwoord = Console.ReadLine();
+
+// Controleren //
+
+if (wachtwoord.Length >= 8 && !wachtwoord.Contains(" "))
+{
+    Console.WriteLine("Uw wachtwoord is geldig.");
+}
+else
+{
+    if (wachtwoord.Contains(" "))
+    {
+        System.Console.WriteLine("Uw wachtwoord mag geen spacies bevatten.");
+    }
+    if (wachtwoord.Length < 8)
+    {
+        System.Console.WriteLine("Uw wachtwoord moet minstens 8 karakters hebben.");
+    }
+}
