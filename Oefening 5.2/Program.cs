ffc395f0aaee6bf3329c223214e8e7cb98383cb6
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2025-12-10 11:50:40 +0100

    Added new project Oefening 5.2 and updated other files.

---
+++ Oefening 5.2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Oefening 5.2: Grootste van drie
+/* Vraag de gebruiker om drie getallen in te voeren. Bepaal en toon welk getal het grootste is.
+Tip: Gebruik geneste if-statements of logische operatoren! */
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Input //
+        Console.WriteLine("=== Grootste van drie ===");
+        int[] getallen = new int[3];
+        // for loop voor 3 drie getallen
+        Console.WriteLine("Voer in 3 getallen: ");
+        for (int i = 0; i < getallen.Length; i++)
+        {
+            getallen[i] = int.Parse(Console.ReadLine());
+        }
+        // check if input works
+        /* for (int i = 0; i < getallen.Length; i++)
+        {
+            Console.WriteLine(getallen[i]);
+        } */
+        int grootste = getallen[0];
+        // Conditionals //
+        for (int i = 0; i < getallen.Length; i++)
+        {
+            if (getallen[i] > grootste)
+            {
+                grootste = getallen[i];
+            }
+            
+        }
+        Console.WriteLine($"\nHet grootste getal is: {grootste}");
+    }
+}
